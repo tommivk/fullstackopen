87368ae64a4b06a5f1fcbd7dd17bd0e77242ab0a
Tommi Kokkonen <tommikokk@gmail.com>
2020-03-17 03:57:17 +0200

    complete 2.9

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -9,11 +9,13 @@ const App = () => {
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ newFilter, setNewFilter ] = useState('')
 
 
   const addPerson = (event) => {
+
     event.preventDefault()
-    
+ 
     if(persons.some(person => person.name.toLowerCase() === newName.toLowerCase())){
         alert(`${newName} is already added to phonebook`)
     }else{
@@ -36,13 +38,17 @@ const App = () => {
   const handleNameChange = (event) => {
       setNewName(event.target.value)
   }
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
+}
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Filter newFilter = {newFilter} handleFilterChange={handleFilterChange}/>
       <form onSubmit = {addPerson}>
         <div>
-          name: <input value={newName} onChange={handleNameChange}/> <br />
+          name: <input value={newName} onChange={handleNameChange} /> <br />
           number: <input value = {newNumber} onChange={handleNumberChange}/>
         </div>
         <div>
@@ -50,20 +56,53 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((p)=><Number key = {p.name} name = {p.name} number = {p.number} />)}
+      <Numbers persons = {persons} filter = {newFilter}/>
+      
     </div>
   )
+  }
 
-}
 
-const Number = (props) => {
+const Filter = (props) => {
+    
+ 
     return(
         <div>
-            
-           <p>{props.name} {props.number}</p>
-            
+            <strong>Filter by name</strong>
+
+            <input value = {props.newFilter} onChange = {props.handleFilterChange} />
+           
+            <br />
+            <br />
         </div>
     )
+}
+
+const Numbers = ({persons, filter}) => {
+   
+    if(filter===""){
+    return(
+        <div>
+            {persons.map((p)=><Person key = {p.name} name = {p.name} number = {p.number} />)}
+        </div>
+    )
+    }else{
+        return(
+            <div>
+               {persons.filter((x)=>x.name.toLowerCase()
+               .includes(filter.toLowerCase())).map((m) => 
+               <Person key = {m.name} name = {m.name} number = {m.number}/>)}
+            </div>
+        )
+    }
 } 
+const Person = ({name, number}) => {
+
+    return(
+        <div>
+            <p>{name} {number}</p>
+        </div>
+    )
+}
 
 export default App
