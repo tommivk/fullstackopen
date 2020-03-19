2e9ba291282c3a2f937da28d31f19a2c47ea1284
Tommi Kokkonen <tommikokk@gmail.com>
2020-03-20 00:31:43 +0200

    complete 2.16

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState , useEffect } from 'react'
 import axios from 'axios'
+import personService from './services/persons'
 const App = () => {
   const [ persons, setPersons ] = useState([]) 
   const [ newName, setNewName ] = useState('')
@@ -7,8 +8,9 @@ const App = () => {
   const [ newFilter, setNewFilter ] = useState('')
 
   useEffect(()=>{    
-      axios.get('http://localhost:3001/persons').then(response=>{
-      setPersons(response.data)
+      personService.getAll().then(response=>{
+      setPersons(response)
+      console.log(response)
     })
   },[])
 
@@ -25,8 +27,8 @@ const App = () => {
         number: newNumber
     }
     
-    axios.post('http://localhost:3001/persons', newNameObject).then(response => {
-      setPersons(persons.concat(response.data),
+    personService.create(newNameObject).then(response => {
+      setPersons(persons.concat(response),
       setNewName(''),
       setNewNumber('')
       )})
