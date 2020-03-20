d2392098cc7b8eaf9bc0b0c2a9fa5a701325c515
Tommi Kokkonen <tommikokk@gmail.com>
2020-03-20 15:04:03 +0200

    complete 2.14

--- osa2/maiden_tiedot/src/index.js
+++ osa2/maiden_tiedot/src/index.js
@@ -2,40 +2,72 @@ import React,{useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios'
 
+const api_key = process.env.REACT_APP_API_KEY
 
 const App = ()=>{
     const [countries, setCountries] = useState([])
     const [filter, setFilter] = useState()
+    const [weather, setWeather] = useState([])
+    const [ country, setCountry ] = useState('Helsinki')
 
-    useEffect(() => {
+        
+        useEffect(() => {
         axios.get('https://restcountries.eu/rest/v2/all').then(response => {
             setCountries(response.data)
             
         })
     },[])
 
+   
+    useEffect (() => { 
+        axios.get(`http://api.weatherstack.com/current?access_key=${api_key}&query=${country}`).then(response => {
+             setWeather(response.data)
+             
+         })
+        },[country])
+   
+    
+   
     const handleFilterChange = (event) => {
         setFilter(event.target.value)
     }
+    const changeCountry = (c) =>{
+        setCountry(c)
+    }
+   
+    
     return(
         <div>
             <FilterForm onChange = {handleFilterChange}/>
-            <Filter setFilter = {setFilter} filterValue = {filter} countries = {countries}/>
-   
+            <Filter setFilter = {setFilter} filterValue = {filter} countries = {countries} changeCountry = {changeCountry} weather = {weather}/>
         </div>
     )
 }
 
-const Filter = ({filterValue, countries, setFilter}) => {
+const WeatherInfo = ({weather}) => {
+    
+    
+    return(
+        <div>
+            <p>Temperature: {weather.current.temperature} Celcius</p>
+        <img src={weather.current.weather_icons[0]} alt='pic'></img>
+        <p>Wind: {weather.current.wind_speed} km/s Wind direction: {weather.current.wind_dir}</p>
+        <p>Humidity: {weather.current.humidity}%</p>
+        </div>
+    )
+}
+const Filter = ({filterValue, countries, setFilter, changeCountry, weather}) => {
     let filtered = countries
     if(filterValue){
     filtered = countries.filter((x)=>x.name.toLowerCase().includes(filterValue.toLowerCase()))
 }
-    console.log(filtered)
+   
     
     if(filtered.length===1){
+        
         return(
-            <ShowSingleCountry country = {filtered[0]}/>
+            
+            <ShowSingleCountry country = {filtered[0]} changeCountry = {changeCountry} weather = {weather} />
         )
     }
     if(filtered.length>10){
@@ -91,12 +123,14 @@ const ShowNames = (props) => {
 }
 
 
-
-
-const ShowSingleCountry = ({country}) => {
-    
+const ShowSingleCountry = ({country, weather, changeCountry}) => {
+     
     return(
         <div>
+            {useEffect((()=>{
+      changeCountry(country.capital)
+  }),[country, changeCountry])
+    }
             <h1>{country.name}</h1>
             <p>capital {country.capital}</p>
             <p>population {country.population}</p>
@@ -105,6 +139,10 @@ const ShowSingleCountry = ({country}) => {
                  {country.languages.map((x)=> <li key= {x.name}>{x.name}</li>)}
                 </ul>
             <img src = {country.flag} alt = 'flag' height = '112px' width = '200px'></img>
+
+            <WeatherInfo weather = {weather}/>
+            
+            
         </div>
     )
 }
