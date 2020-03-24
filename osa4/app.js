b3a16d67548161819d6bef1691e6acc6837f2a42
Tommi Kokkonen <tommikokk@gmail.com>
2020-03-24 22:54:09 +0200

    complete 4.2

---
+++ osa4/app.js
@@ -0,0 +1,18 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blogs')
+
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true})
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api', blogsRouter)
+
+module.exports = app
