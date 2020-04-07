94124a09daa030877d77176d8f1f81263acc04cb
Tommi Kokkonen <tommikokk@gmail.com>
2020-04-07 18:15:58 +0300

    complete 6.19

--- osa6/redux-anecdotes/src/components/AnecdoteList.js
+++ osa6/redux-anecdotes/src/components/AnecdoteList.js
@@ -1,36 +1,57 @@
 import React from 'react'
 import { addVote } from '../reducers/anecdoteReducer'
-import { useSelector, useDispatch } from 'react-redux'
+import { connect } from 'react-redux'
 import { changeNotification } from '../reducers/notificationReducer'
 
-const AnecdoteList = () => {
-  const anecdotes = useSelector((state) => {
-    if (!state.filter) {
-      return state.anecdotes
+const AnecdoteList = (props) => {
+  console.log(props.anecdotes)
+  const anecdotes = () => {
+    if (!props.filter) {
+      return props.anecdotes
     }
-    return state.anecdotes.filter((x) =>
-      x.content.toLowerCase().includes(state.filter.toLowerCase())
+    return props.anecdotes.filter((x) =>
+      x.content.toLowerCase().includes(props.filter.toLowerCase())
     )
-  })
-
-  const dispatch = useDispatch()
+  }
 
   const vote = (id) => {
     console.log('vote', id)
-    const anecdote = anecdotes.filter((x) => x.id === id)
-    dispatch(addVote(anecdote[0]))
-    dispatch(changeNotification(`you voted '${anecdote[0].content}'`, 5))
+    const anecdote = props.anecdotes.filter((x) => x.id === id)
+    props.addVote(anecdote[0])
+    props.changeNotification(`you voted '${anecdote[0].content}'`, 5)
   }
 
-  return anecdotes.map((anecdote) => (
-    <div key={anecdote.id}>
-      <div>{anecdote.content}</div>
-      <div>
-        has {anecdote.votes}
-        <button onClick={() => vote(anecdote.id)}>vote</button>
-      </div>
+  return (
+    <div>
+      {anecdotes().map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
     </div>
-  ))
+  )
 }
 
-export default AnecdoteList
+const mapStateToProps = (state) => {
+  return {
+    anecdotes: state.anecdotes,
+    filter: state.filter,
+    message: state.message,
+  }
+}
+
+const mapDispatchToProps = {
+  addVote,
+  changeNotification,
+}
+
+const ConnectedAnecdoteList = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AnecdoteList)
+
+export default ConnectedAnecdoteList
