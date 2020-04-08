25cf59b9498e45b32634b70a6e9caba6666c222b
Tommi Kokkonen <tommikokk@gmail.com>
2020-04-08 21:32:56 +0300

    complete 7.7

---
+++ osa7/country-hook/src/App.js
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange,
+  }
+}
+
+const useCountry = (name) => {
+  const url = `https://restcountries.eu/rest/v2/name/${name}?fullText=true`
+  const [country, setCountry] = useState(null)
+
+  useEffect(() => {
+    if (name === null || name === '') {
+      return
+    } else {
+      axios
+        .get(url)
+        .then((response) => {
+          setCountry(response)
+        })
+        .catch((error) => setCountry('null'))
+    }
+  }, [url, name])
+
+  return country
+}
+
+const Country = ({ country }) => {
+  console.log(country)
+  if (!country) {
+    return null
+  }
+
+  if (!country.data) {
+    return <div>not found...</div>
+  }
+
+  return (
+    <div>
+      <h3>{country.data[0].name} </h3>
+      <div>capital {country.data[0].capital} </div>
+      <div>population {country.data[0].population}</div>
+      <img
+        src={country.data[0].flag}
+        height='100'
+        alt={`flag of ${country.data[0].name}`}
+      />
+    </div>
+  )
+}
+
+const App = () => {
+  const nameInput = useField('text')
+  const [name, setName] = useState('')
+  const country = useCountry(name)
+
+  const fetch = (e) => {
+    e.preventDefault()
+    setName(nameInput.value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
+
+      <Country country={country} />
+    </div>
+  )
+}
+
+export default App
